f27150b8b967e4e6dc0ac7ebeaa39bd5ff10b096
thediogen <evgendvodnenko@gmail.com>
2025-05-25 20:56:23 +0300

    added ability to create orders

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -11,3 +12,28 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk=pk) 
+
+
+@app.post('/orders')
+async def create(request: Request):
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    return order
